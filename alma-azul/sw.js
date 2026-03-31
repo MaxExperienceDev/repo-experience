5172131f1a4ba0e57ae00035af6e9a1b4c2d454b
MaxExperienceDev <GitMax23091966@gmail.com>
2026-03-30 19:55:58 -0500

    update

--- alma-azul/sw.js
+++ alma-azul/sw.js
@@ -1,18 +1,15 @@
 'use strict';
 
-var VERSION = '2026.03.30.19.33.05';
+var VERSION = '2026.03.30.19.49.56';
 const CACHE_NAME = `cache-v${VERSION}`;
 var CORE_ASSETS = [
   "./assets/css/style.css",
-  "./assets/img/icons/apple-splash-1170x2532.png",
   "./assets/img/icons/apple-splash-1179x2556.png",
   "./assets/img/icons/apple-splash-1290x2796.png",
   "./assets/img/icons/apple-splash-750x1334.png",
   "./assets/img/icons/apple-touch-icon-180.png",
-  "./assets/img/icons/favicon-48.png",
-  "./assets/img/icons/icon-192-maskable.png",
+  "./assets/img/icons/favicon.ico",
   "./assets/img/icons/icon-192.png",
-  "./assets/img/icons/icon-512-maskable.png",
   "./assets/img/icons/icon-512.png",
   "./assets/img/icons/icon-android.png",
   "./assets/img/icons/og.png",
